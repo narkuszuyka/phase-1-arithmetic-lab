2a9c2ba998b519ecca972966814d2b7c5f19820a
Sukran <sukran.akyuz.93@gmail.com>
2023-04-01 12:19:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 4
+let num2 = 15.5
+num1 * num2
+ let multiply = num1 * num2;
 
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = num1;
+let num4 = num2;
+let mod = num1 % num2;
+
+let max = Math.max(num1, num2, 20);
 // Write your code here
